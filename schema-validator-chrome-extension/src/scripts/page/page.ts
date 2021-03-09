921017530e2f2c41dc4166f5514b6b83e0b33f0b
Hans-Christiaan <hans-christiaan@yoast.com>
2021-03-09 09:00:48 +0100

    First version of gathering schema from webpage.

---
+++ schema-validator-chrome-extension/src/scripts/page/page.ts
@@ -0,0 +1,14 @@
+
+function initialize() {
+    const schemaElements = document.querySelectorAll( "script[type='application/ld+json']" );
+
+    const schemas = [];
+
+    for( let i = 0; i < schemaElements.length; i++ ) {
+        schemas.push( JSON.parse( schemaElements[i].innerHTML ) );
+    }
+
+    console.log( schemas );
+}
+
+initialize();
